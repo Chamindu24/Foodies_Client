c82f7701f391c74d2a31c217b24bffd1de665f05
Chamindu24 <chamindus.22@cse.mrt.ac.lk>
2025-07-01 00:08:07 +0530

    Add initial components and styles for the food ordering application
    - Added logo image to assets.
    - Created ExploreMenu component for exploring food options.
    - Developed Header component with navigation links and a call-to-action button.
    - Implemented Menubar component with logo, navigation links, and user action buttons.
    - Added ContactUs page for user inquiries.
    - Created ExploreFood page for food exploration.
    - Developed Home page integrating Header and ExploreMenu components.
    - Added CSS styles for Menubar component.

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+
+const Home = () => {
+  return (
+    <div>
+     <main className='container'>
+        <Header />
+        <ExploreMenu />
+     </main>
+    </div>
+  )
+}
+
+export default Home
