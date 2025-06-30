5b58e80ef5e1563433b156c2cf2c723e9b54a934
Chamindu24 <chamindus.22@cse.mrt.ac.lk>
2025-07-01 00:53:28 +0530

    Add initial CSS file for ExploreMenu component

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,7 +1,47 @@
 import logo from './logo.png';
 import cart from './cart.jpg';
+import biriyani from './biriyani.png';
+import pizza from './pizza.png';
+import burger from './burger.png';
+import cake from './cake.png';
+import icecream from './icecream.png';
+import salad from './salas.png';
+import rolls from './rolls.jpg';
+
 
 export const assets = {
     logo,
     cart
+    
 };
+
+export const categories = [
+    {
+        category: 'Biriyani',
+        icon: biriyani
+    },
+    {
+        category: 'Pizza',
+        icon: pizza
+    },
+    {
+        category: 'Burger',
+        icon: burger
+    },
+    {
+        category: 'Cake',
+        icon: cake
+    },
+    {
+        category: 'Ice Cream',
+        icon: icecream
+    },
+    {
+        category: 'Salad',
+        icon: salad
+    },
+    {
+        category: 'Rolls',
+        icon: rolls
+    }
+]
--- src/components/ExploreMenu/ExploreMenu.jsx
+++ src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import { categories } from '../../assets/assets'
+import './ExploreMenu.css'
 
 const ExploreMenu = () => {
   return (
-    <div>
-      ExploreMenu
+    <div className='explore-menu position-relative'>
+      <h1 className='d-flex justify-content-between align-items-center '>
+        Explore Our Menu
+        <div className='d-flex'>
+            <i className='bi bi-arrow-left-circle scroll-icon'></i>
+            <i className='bi bi-arrow-right-circle scroll-icon'></i>
+        </div>
+
+      </h1>
+      <p>Explore curated lists of dishes from top categories </p>
+      <div className='explore-menu-list d-flex  gap-4 justify-content-between overflow-auto'>
+        {
+            categories.map((item, index) => {
+                return (
+                    <div key={index} className='explore-menu-list-item text-center'>
+                        <img src={item.icon} alt="" className='rounded-circle' height={128} width={128} />
+                        <p className='mt-2 fw-bold '>{item.category}</p>
+
+                    </div>
+                )
+            })
+        }
+
+      </div>
     </div>
   )
 }
